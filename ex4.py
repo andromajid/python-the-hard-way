538ac6337655a5b2ee26f9bf0562e6d7a960c7ee
appleseed <andromajid@gmail.com>
2013-07-03 14:35:40 +0700

    add to ex4

---
+++ ex4.py
@@ -0,0 +1,9 @@
+cars = 100
+space_in_a_car = 1.0
+drivers = 30
+passanger = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
