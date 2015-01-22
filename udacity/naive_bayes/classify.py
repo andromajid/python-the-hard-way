ce52f05e2648febb101e2bb4ccb28468bc4bc9bf
andro_majid <andro.majid@redawning.com>
2015-01-22 12:50:21 +0700

    add udacity

---
+++ udacity/naive_bayes/classify.py
@@ -0,0 +1,22 @@
+from sklearn.naive_bayes import GaussianNB
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+
+    ### create classifier
+    clf = GaussianNB()#TODO
+    clf.fit(features_train, labels_train)
+    ### fit the classifier on the training features and labels
+    #TODO
+
+    ### use the trained classifier to predict labels for the test features
+    pred =  clf.predict(features_test) #TODO
+
+
+    ### calculate and return the accuracy on the test data
+    ### this is slightly different than the example,
+    ### where we just print the accuracy
+    ### you might need to import an sklearn module
+    accuracy = #TODO
+    return accuracy__author__ = 'arkananta'
